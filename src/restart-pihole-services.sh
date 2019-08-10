b5b3ef130f9d89fe5eac6cbf4a276b7db990375b
Faizal Hasanwala <faizalhasanwala@gmail.com>
2019-08-10 14:40:21 +0530

    Update pihole setup files

--- src/restart-pihole-services.sh
+++ src/restart-pihole-services.sh
@@ -6,13 +6,13 @@ if [ ! -z "$1" ]; then
 	serviceCMD=$1;
 fi
 
-service pihole-FTL "$serviceCMD" &&\
+service pihole-FTL "$serviceCMD" && \
 	service lighttpd "$serviceCMD"
 
 if [ "$2" = "keep-alive" ]; then
 	while true
 	do
-		timeout 2 dig @127.0.0.1 pi.hole || exit 1
+		timeout 2 dig @127.0.0.1 example.org || exit 1
 		sleep 5
 	done
 fi
